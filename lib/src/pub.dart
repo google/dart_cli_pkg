9156c781d1c60011f223d39d86d89ebeb0255bda
Simon Binder <oss@simonbinder.eu>
2026-06-22 23:03:19 +0200

    Fix deprecation warning

--- lib/src/pub.dart
+++ lib/src/pub.dart
@@ -38,7 +38,7 @@ final String _credentialsPath = () {
   // This follows the same logic as pub.
   if (dartVersion >= Version.parse('2.15.0')) {
     // https://github.com/dart-lang/pub/blob/a16763a93b5050c6a9d917fca5a132cfcb00f1a9/doc/cache_layout.md#layout
-    return p.join(applicationConfigHome('dart'), 'pub-credentials.json');
+    return p.join(BaseDirectories('dart').configHome, 'pub-credentials.json');
   }
 
   // https://github.com/dart-lang/pub/blob/d99b0d58f4059d7bb4ac4616fd3d54ec00a2b5d4/lib/src/system_cache.dart#L34-L43
